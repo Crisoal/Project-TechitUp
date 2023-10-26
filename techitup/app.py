67d6274a66055928f8ce034927051015dcc772a4
Christian Adebambo <c.adebambo@alustudent.com>
2023-10-27 00:55:55 +0400

    makeitbetter

--- techitup/app.py
+++ techitup/app.py
@@ -9,8 +9,21 @@ openai.api_key = "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"
 # Database setup
 conn = sqlite3.connect('users.db')
 c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)''')
+c.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        username TEXT PRIMARY KEY, 
+        password TEXT, 
+        interest TEXT, 
+        goal TEXT,
+        assessment_score INTEGER
+    )
+''')
 conn.commit()
+
+# At the beginning of the script, initialize the session state for username if it doesn't exist yet
+if 'username' not in st.session_state:
+    st.session_state.username = None
+
 
 def get_gpt_response(prompt):
     response = openai.ChatCompletion.create(
@@ -22,11 +35,12 @@ def get_gpt_response(prompt):
     )
     return response['choices'][0]['message']['content']
 
-def register_user(username, password):
+def register_user(username, password, interest, goal):
     hashed_pw = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     with sqlite3.connect('users.db') as conn:
         c = conn.cursor()
-        c.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashed_pw))
+        c.execute("INSERT INTO users (username, password, interest, goal) VALUES (?, ?, ?, ?)", 
+                  (username, hashed_pw, interest, goal))
         conn.commit()
 
 def check_user(username, password):
@@ -38,14 +52,22 @@ def check_user(username, password):
             return True
     return False
 
+def store_assessment_result(username, score):
+    with sqlite3.connect('users.db') as conn:
+        c = conn.cursor()
+        c.execute("UPDATE users SET assessment_score = ? WHERE username = ?", (score, username))
+        conn.commit()
+
 def registration_page():
     st.title("Register")
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
     confirm_password = st.text_input("Confirm Password", type="password")
+    interest = st.text_input("Your primary coding interest (e.g. Web Development, Data Science, etc.)")
+    goal = st.text_area("What are your coding goals?")
     if st.button("Register"):
         if password == confirm_password:
-            register_user(username, password)
+            register_user(username, password, interest, goal)
             st.success("Registration successful!")
         else:
             st.error("Passwords do not match!")
@@ -57,39 +79,73 @@ def login_page():
     if st.button("Login"):
         if check_user(username, password):
             st.success("Logged in successfully!")
+            st.session_state.username = username  # Update the session state
+            return "Chat"  # Return the next page to navigate to
         else:
             st.error("Invalid username or password")
+    return "Login"  # By default, stay on the Login page
 
-def assessment_page():
+def assessment_page(username):
     st.title("Initial Assessment")
-    q1 = st.radio("Which language is used for web apps?", ["Python", "JavaScript", "C++", "Java"])
-    q2 = st.radio("Which of the following is a database?", ["MySQL", "HTML", "CSS", "JS"])
+    questions = {
+        "Which language is used for web apps?": ["Python", "JavaScript", "C++", "Java"],
+        "Which of the following is a database?": ["MySQL", "HTML", "CSS", "JS"],
+        "What does HTML stand for?": ["High Transfer Machine Language", "Hyperlinking Text Management Language", "Hyper Text Markup Language", "Hyper Transfer Markup Language"],
+        "Which of the following is not an OOP principle?": ["Inheritance", "Polymorphism", "Duplication", "Encapsulation"],
+        "What command is used to install packages in Python?": ["npm install", "yarn add", "pip install", "dotnet add"]
+    }
+    correct_answers = {
+        "Which language is used for web apps?": "JavaScript",
+        "Which of the following is a database?": "MySQL",
+        "What does HTML stand for?": "Hyper Text Markup Language",
+        "Which of the following is not an OOP principle?": "Duplication",
+        "What command is used to install packages in Python?": "pip install"
+    }
+    user_answers = {}
+    for question, options in questions.items():
+        user_answers[question] = st.radio(question, options)
     if st.button("Submit"):
-        # TODO: Evaluate the answers and assign a level to the user
-        st.success("Assessment complete!")
+        correct_count = sum([1 for question, answer in user_answers.items() if answer == correct_answers[question]])
+        store_assessment_result(username, correct_count)  # Store the user's score
+        st.success(f"You answered {correct_count} out of {len(questions)} questions correctly!")
+        if correct_count > len(questions) // 2:
+            st.write("Great job! You have a good understanding of basic programming concepts.")
+        else:
+            st.write("Keep practicing! You'll get better with time.")
 
-def chatbot_interface():
+def chatbot_interface(username=None):
     st.title("TechItUp AI-Powered Coding Learning Chatbot")
     st.write("Welcome to the TechItUp AI-powered coding learning chatbot!")
     st.write("This chatbot will assist you in learning coding concepts. Ask any programming-related questions, and let's get started!")
-    
-    # Links for additional resources
     st.markdown("[Click here for a coding challenge](https://edabit.com/challenges)")
     st.markdown("[Start a quiz](https://www.codeconquest.com/coding-quizzes/)")
-
     user_input = st.text_input("Type your question here...")
     if user_input:
+        with sqlite3.connect('users.db') as conn:
+            c = conn.cursor()
+            c.execute("SELECT assessment_score FROM users WHERE username = ?", (username,))
+            score = c.fetchone()[0]
+            if score and score < 3:
+                user_input = f"beginner {user_input}"
         with st.spinner('Processing...'):
             chatbot_response = get_gpt_response(user_input)
         st.write(f"Chatbot: {chatbot_response}")
 
 if __name__ == "__main__":
-    page = st.sidebar.selectbox("Choose a page", ["Login", "Register", "Assessment", "Chat"])
+    default_page = "Chat" if st.session_state.username else "Login"  # Default to Chat if logged in
+    page = st.sidebar.selectbox("Choose a page", ["Login", "Register", "Assessment", "Chat"], index=["Login", "Register", "Assessment", "Chat"].index(default_page))
+    
     if page == "Login":
-        login_page()
+        next_page = login_page()
+        if next_page != page:
+            page = next_page
+            st.experimental_rerun()  # Rerun the app to update the page
     elif page == "Register":
         registration_page()
     elif page == "Assessment":
-        assessment_page()
+        if st.session_state.username:
+            assessment_page(st.session_state.username)
+        else:
+            st.warning("Please log in before taking the assessment.")
     elif page == "Chat":
-        chatbot_interface()
+        chatbot_interface(st.session_state.username)
